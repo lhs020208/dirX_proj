bf6a4a4c9d5d8be1c74d857f54755a67e4cd3f42
lhs020208 <92709240+lhs020208@users.noreply.github.com>
2025-03-25 20:32:17 +0900

    따라하기 6-1 완

--- LabProject01/Shaders.hlsl
+++ LabProject01/Shaders.hlsl
@@ -1,17 +1,40 @@
+
 float4 VSMain(uint nVertexID : SV_VertexID) : SV_POSITION
 {
-    float4 output;
+    float4 output = (float4) 0;
     if (nVertexID == 0)
-        output = float4(0.0, 0.5, 0.5, 1.0);
+        output = float4(-1.0f, +1.0f, 0.0f, 1.0f);
     else if (nVertexID == 1)
-        output = float4(0.5, -0.5, 0.5, 1.0);
+        output = float4(+1.0f, +1.0f, 0.0f, 1.0f);
     else if (nVertexID == 2)
-        output = float4(-0.5, -0.5, 0.5, 1.0);
+        output = float4(+1.0f, -1.0f, 0.0f, 1.0f);
+    else if (nVertexID == 3)
+        output = float4(-1.0f, +1.0f, 0.0f, 1.0f);
+    else if (nVertexID == 4)
+        output = float4(+1.0f, -1.0f, 0.0f, 1.0f);
+    else if (nVertexID == 5)
+        output = float4(-1.0f, -1.0f, 0.0f, 1.0f);
     return (output);
 }
+#define FRAME_BUFFER_WIDTH 640.0f
+#define FRAME_BUFFER_HEIGHT 480.0f
+#define HALF_WIDTH (FRAME_BUFFER_WIDTH * 0.5f)
+#define HALF_HEIGHT (FRAME_BUFFER_HEIGHT * 0.5f)
+#define EPSILON 1.0e-5f
 
 float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
-//프리미티브의 모든 픽셀의 색상을 노란색으로 반환한다. 
-    return (float4(1.0f, 1.0f, 0.0f, 1.0f)); //Yellow
+    float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
+    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) -
+0.5f; // (0, 1) : (-0.5, 0.5)
+    f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
+    float fRadius = 0.3f;
+    float fRadian = radians(360.0f / 30.0f);
+    for (float f = 0; f < 30.0f; f += 1.0f)
+    {
+        float fAngle = fRadian * f;
+        cColor.rgb += (0.0025f / length(f2NDC + float2(fRadius * cos(fAngle), fRadius *
+sin(fAngle))));
+    }
+    return (cColor);
 }
